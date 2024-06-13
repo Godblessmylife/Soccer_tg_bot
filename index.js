7a255b8baf9235ada882b1d329a110c74fd4125a
Hacker1993 <fillagucci@gmail.com>
2024-06-13 13:18:45 +0400

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+const TelegramBot = require("node-telegram-bot-api')
+
+const TOKEN = '7428631680:AAGMkgYCV4ORZ2VbtaqBldxNvJKEoiHQNkY'
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, 'Hello This Is First Soccer Game on Telegram, bot says: "Hi, ${msg.from.first_name}"')
+})
